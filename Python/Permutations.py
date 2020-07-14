551d9b89b16cf97845de8a536b5daf339053becd
Christopher Nguyen <chrisngyn99@gmail.com>
2020-07-13 22:23:53 -0400

    cant solve permutations :(

---
+++ Python/Permutations.py
@@ -0,0 +1,29 @@
+"""
+Given a collection of distinct integers, return all possible permutations.
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+"""
+
+from typing import List
+
+def permuteIterative(nums: List[int]) -> List[List[int]]:
+    ret = []
+    return ret
+
+
+def permuteRecursive(nums: List[int]) -> List[List[int]]:
+    ret = []
+    return ret
+
+
+print(permuteIterative([1, 2, 3]))
+print(permuteRecursive([1, 2, 3]))
