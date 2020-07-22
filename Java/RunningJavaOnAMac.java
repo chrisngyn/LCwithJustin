a32d47f60dca00933e7c87ecb08b12ec98bf82dd
Christopher Nguyen <chrisngyn99@gmail.com>
2020-07-22 00:41:00 -0400

    java on a mac

---
+++ Java/RunningJavaOnAMac.java
@@ -0,0 +1,8 @@
+public class RunningJavaOnAMac {
+    public static void main(String[] args) {
+        System.out.println("1. javac RunningJavaOnAMac.java");
+        System.out.println("2. Will generate 'RunningJavaOnAMac.class'");
+        System.out.println("3. java RunningJavaOnAMac");
+        System.out.println("4. Alternatively use IntelliJ :^)");
+    }
+}
