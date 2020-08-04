41e234e2c701e32200716d8f088d995a01349d78
Christopher Nguyen <chrisngyn99@gmail.com>
2020-08-03 23:36:43 -0400

    CLONE GRAPHHHHHH

--- Python/Graphs/CloneGraph.py
+++ Python/Graphs/CloneGraph.py
@@ -1,5 +1,5 @@
 """
-All I got to say is - I HATE GRAPHS
+all I got to say is - I HATE GRAPHS
 
 so, all trees are graphs. but not all graphs are trees!
 
@@ -31,52 +31,59 @@ class GraphNode:
         self.neighbors = neighbors if neighbors is not None else []
     
 def cloneGraph(node: GraphNode) -> GraphNode:
-    ret = GraphNode(node.val)
-    dummy = ret
+    if node == None:
+        return None
+    
+    clones = {node : GraphNode(node.val)}  # map original node to cloned node
     queue = deque([node])
-
+    
     while len(queue) > 0:
-        current_node = queue.popleft()
-        dummy = GraphNode(current_node.val)
+        current_node = queue.popleft()  # this is the ORIGINAL node. first, check if in dictionary, if not, add, append neighbours to the clone
 
         for item in current_node.neighbors:
-            new_node = GraphNode(item.val)
-            dummy.neighbors.append(item)
-            queue.append(item)
-        
-        ret.neighbors.append(dummy)
+            if item not in clones:  # if there's a neighbour of our original node that isn't in the dictionary...
+                clones[item] = GraphNode(item.val)  # we'll add it, and map it to a deep copy
+                queue.append(item)
+            
+            clones[current_node].neighbors.append(clones[item])  # clones[item] gives us the deep copy of the node, so then we append all the deep copy neighbours to it
     
-    return ret
+    return clones[node]
 
 def printGraphBFS(node: GraphNode):
     queue = deque([node])
+    seen = set([node])
 
     while len(queue) > 0:
-        current = queue.popleft()
-        print(current.val)
+        current_node = queue.popleft()
 
-        for item in current.neighbors:
-            queue.append(item)
+        print(current_node.val)
 
+        for item in current_node.neighbors:
+            if item not in seen:
+                queue.append(item)
+                seen.add(item)
 
-root = GraphNode(1)
+"""
+1 -- 3
+     |
+2 -- 4
+
+expected: 1, 3, 4, 2
+
+we'll make a deep clone of this, then we'll modify the original graph, then we'll print BFS of both graphs and see if they're different
+"""
+node1 = GraphNode(1)
 node2 = GraphNode(2)
 node3 = GraphNode(3)
 node4 = GraphNode(4)
-root.neighbors = [node3]
-node3.neighbors = [node4]
-node4.neighbors = [node2]
-deep_copy = cloneGraph(root)
+node1.neighbors = [node3]
+node2.neighbors = [node4]
+node3.neighbors = [node1, node4]
+node4.neighbors = [node2, node3]
+deep_copy = cloneGraph(node1)
 node2.val = 99
-print('NORMAL GRAPH (with modified 99)')
-printGraphBFS(root)
-print('DEEP COPY')
+print("DEEP COPY GRAPH:")
 printGraphBFS(deep_copy)
-
-"""
-1 ----- 3
-        |       
-2 ----- 4
-
-expected 1 - 3 - 4 - 2
-"""
+print("")
+print("MODIFIED ORIGINAL GRAPH:")
+printGraphBFS(node1)
--- Python/Graphs/TraverseGraphBFS.py
+++ Python/Graphs/TraverseGraphBFS.py
@@ -1,54 +1,63 @@
-"""
-Traversing a graph using Breadth First Search.
-"""
+# Traversing a graph with Breadth First Search.
+# Generally the same approach as with a tree, except you need to store all your seen nodes so you don't revisit them.
 
 from collections import deque
 
 class GraphNode:
-    def __init__(self, val=0, neighbors=None):
+    def __init__(self, val=0, neighbours=None):
         self.val = val
-        # if neighbors == None:
-        #     self.neighbors = []
-        # else:
-        #     self.neighbors = neighbors
-        self.neighbors = neighbors if neighbors is not None else []
+        if neighbours == None:
+            self.neighbours = []
+        else:
+            self.neighbours = neighbours
 
 def printGraphBFS(node: GraphNode):
     queue = deque([node])
-
-    while len(queue) > 0:
-        # WAIT OMG THIS DOESN'T WORK IF IT'S ALL CONNECTED NOOOOO WTF
-        # ok rip iwant2 die
-
-
-    queue = deque([node])
+    seen = set([node])
 
     while len(queue) > 0:
         current_node = queue.popleft()
 
         print(current_node.val)
 
-        for node in current_node.neighbors:
-            queue.append(node)
-
-root = GraphNode(1)
-node2 = GraphNode(2)
-node3 = GraphNode(3)
-node4 = GraphNode(4)
-root.neighbors = [node3]
-node3.neighbors = [node4]
-node4.neighbors = [node2]
-deep_copy = cloneGraph(root)
-node2.val = 99
-print('NORMAL GRAPH (with modified 99)')
-printGraphBFS(root)
-print('DEEP COPY')
-printGraphBFS(deep_copy)
+        for item in current_node.neighbours:
+            if item not in seen:
+                queue.append(item)
+                seen.add(item)
 
 """
-1 ----- 3
-        |       
-2 ----- 4
+1 -- 2    5
+  \- 3 -/
+  \- 4 -/
 
-expected 1 - 3 - 4 - 2
+expected: 1, 2, 3, 4, 5, shouldn't print 5 twice!
 """
+print("TEST 1")
+t1node1 = GraphNode(1)
+t1node2 = GraphNode(2)
+t1node3 = GraphNode(3)
+t1node4 = GraphNode(4)
+t1node5 = GraphNode(5)
+t1node1.neighbours = [t1node2, t1node3, t1node4]
+t1node2.neighbours = [t1node1]
+t1node3.neighbours = [t1node1, t1node5]
+t1node4.neighbours = [t1node1, t1node5]
+t1node5.neighbours = [t1node3, t1node4]
+printGraphBFS(t1node1)
+print("")
+
+"""
+1 -- 2
+  \  |
+     3
+
+expected: 1, 2, 3, shouldn't print any of the other nodes again
+"""
+print("TEST 2")
+t2node1 = GraphNode(1)
+t2node2 = GraphNode(2)
+t2node3 = GraphNode(3)
+t2node1.neighbours = [t2node2, t2node3]
+t2node2.neighbours = [t2node1, t2node3]
+t2node3.neighbours = [t2node1, t2node2]
+printGraphBFS(t2node1)
