bba618f146e5f107d35ca1a6bb30912949fd295d
Chris N <chrisngyn99@gmail.com>
2020-11-01 23:13:51 -0500

    COMMENTING MINIMUM WINDOW SUBSTRING (LC76)

--- Python/SlidingWindow/MinimumWindowSubstring.py
+++ Python/SlidingWindow/MinimumWindowSubstring.py
@@ -7,13 +7,19 @@ Output: "BANC"
 
 Input: s = "a", t = "a"
 Output: "a"
+
+
+approach - let's say 't' is 'aabb', so we have 4 characters to match. we'll keep a variable 'matched_char' that we increment when we visit a letter we need.
+once matched_char == len(t), we'll start popping off characters until we pop off one we need, where matched_char is decremented
 """
+
+from collections import Counter
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         char_dict = Counter(t)
         matched_char = 0  # need to match len(t)
-        substr_str = 0
+        substr_str = 0  # where does the substring start? how long is it? this is what we need so we can return something in the end
         substr_len = float('inf')
         win_str = 0
         
@@ -23,11 +29,11 @@ class Solution:
             if char in char_dict:
                 char_dict[char] -= 1
                 
-                if char_dict[char] >= 0:
+                if char_dict[char] >= 0:  # if 's' is 'aaaa' and 't' is 'aa', our count would go to -2. meaning we have more characters than needed
                     matched_char += 1
                     
             while matched_char == len(t):
-                if win_end - win_str + 1 < substr_len:
+                if win_end - win_str + 1 < substr_len:  # if window length is smaller than what we currently have, set the length, and set where it begins
                     substr_str = win_str
                     substr_len = win_end - win_str + 1
                     
@@ -40,7 +46,6 @@ class Solution:
                     char_dict[ch] += 1
                 
                 win_str += 1
-        
         
         if substr_len == float('inf'):
             return ''
